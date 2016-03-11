a376b13d250bcced63bab15aef81473ac15bcd1c
ZestyTechStudent <legowarship@gmail.com>
2016-03-10 21:13:20 -0800

    Added files via upload
    JUST DO IT! DO ITTTT! DOWNLOAD THIS VBS AND RUNNNNNNNNN. DONT LET YOUR MEMES BE MEMESSSSSSSS

---
+++ aids.bat
@@ -0,0 +1,102 @@
+@echo off 
+echo enjoy your porn joey :D
+
+
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+msg * "WWW.Pornhub.com would like access to your computer" 
+ping pornhub.com
+traceroute pornhub.com
+@start https://notavirus.com
+@start https://downloadfreeram.com
+echo msgbox "Shrek is love , shrek is life. Joey bends ogre in shreks swAMP!" >> hw.txt
+start hw.txt 
+start Shrek
+start creditcardinfo
+start socialsecuritynumber
+start joeystanleyssearchhistory
+start taxinfofolder
+msg * "All your files belong to us! - Anonymous"
+start yourefucked
+start computersuicide
+msg * "your computer is killing itself"
+shutdown -s -t 40
+msg * "hahahahahah"
+msg * "you're dead"
+@start https://google.com
+@start https://kkk.com
+@start https://anonymous.com
+@start https://lizardsquad.com
+@start https://googlehammer.com
